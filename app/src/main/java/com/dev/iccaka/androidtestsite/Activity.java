7c78d00eeb699fa916b4244f45fe1895b9215585
Hristo Mitsev <dakillaroshun@gmail.com>
2019-12-06 10:59:15 +0200

    'BaseActivity.java' modified. Added a new interface called 'Activity.java'
    Added a new interface called 'Activity.java' which has the newly added
    methods from the previous commit.
    
    Added an '@Override' tag over the three new methods.

---
+++ app/src/main/java/com/dev/iccaka/androidtestsite/Activity.java
@@ -0,0 +1,8 @@
+package com.dev.iccaka.androidtestsite;
+
+public interface Activity {
+
+    void setStringArrayDefault(int stringArrayDefault);
+    void setStringArrayTranslated(int stringArrayTranslated);
+    void putWordsIntoList();
+}
--- app/src/main/java/com/dev/iccaka/androidtestsite/BaseActivity.java
+++ app/src/main/java/com/dev/iccaka/androidtestsite/BaseActivity.java
@@ -6,22 +6,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 
-public abstract class BaseActivity extends AppCompatActivity implements SetViews {
+public abstract class BaseActivity extends AppCompatActivity implements SetViews, Activity {
 
     protected GridView mainGridView;
     protected List<Dictionary> dictionaryList;
     protected int stringArrayDefault;
     protected int stringArrayTranslated;
 
+    @Override
     public void setStringArrayDefault(int stringArrayDefault) {
         this.stringArrayDefault = stringArrayDefault;
     }
 
+    @Override
     public void setStringArrayTranslated(int stringArrayTranslated) {
         this.stringArrayTranslated = stringArrayTranslated;
     }
 
-    protected void putWordsIntoList(){
+    @Override
+    public void putWordsIntoList(){
         String[] numbersDefault = getResources().getStringArray(this.stringArrayDefault);
         String[] numbersTranslated = getResources().getStringArray(this.stringArrayTranslated);
 
