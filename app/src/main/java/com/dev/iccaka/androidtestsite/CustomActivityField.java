681ee1ca0193408d69a8ffe9ada6ef0cad4e6bdc
Hristo Mitsev <dakillaroshun@gmail.com>
2019-12-22 16:28:24 +0200

    Rename 'CustomActivityActivityField.java' to 'CustomActivityField.java'
    The 'CustomActivityActivityField.java' file has been renamed to
    'CustomActivityField.java', because of a typo.

---
+++ app/src/main/java/com/dev/iccaka/androidtestsite/CustomActivityField.java
@@ -0,0 +1,10 @@
+package com.dev.iccaka.androidtestsite;
+
+import android.widget.ImageView;
+
+public class CustomActivityField extends BaseActivityActivityField {
+
+    public CustomActivityField(Dictionary dictionary, ImageView wordImage) {
+        super(dictionary, wordImage);
+    }
+}
